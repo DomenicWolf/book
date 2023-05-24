0be413867d096b3d6f6d966059e2e0f3054e81f3
Domenic <domenicdrw@gmail.com>
2023-05-24 00:08:15 -0400

    add

---
+++ db.js
@@ -0,0 +1,28 @@
+/** Database config for database. */
+
+/*
+const { Client } = require("pg");
+const {DB_URI} = require("./config");
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+
+module.exports = db;*/
+
+const { Client } = require('pg');
+const DB_URI = process.env.NODE_ENV === 'test' ? `postgresql://dom:${'dom123321!'}@127.0.0.1:5432/books_test`: `postgresql://dom:${'dom123321!'}@127.0.0.1:5432/books`;
+ 
+    
+    
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
